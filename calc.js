274469a4a4fbef5010c3b5bee5b1cb773dfcf89b
jalbiston <jacob.albiston@gmail.com>
2021-08-23 15:35:49 -0600

    multiplication functionality added

--- calc.js
+++ calc.js
@@ -25,6 +25,9 @@ reader.question("What would you like to calculate?", function (input) {
   if (mathSymbol === "sqrt") {
     console.log(Math.sqrt(num1));
   }
+  if (mathSymbol === "*") {
+    console.log(num1 * num2);
+  }
   reader.close();
 
   // This line closes the connection to the command line interface.
